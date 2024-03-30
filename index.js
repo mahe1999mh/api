a381f7a62b2fce7ba4edc9affd5eff836fcc2342
mahe1999mh <mahe1999mh@gmail.com>
2024-03-30 23:18:47 +0530

    rm html file

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
 const express = require('express');
-const path = require('path');
 const app = express();
 require('dotenv').config();
 
@@ -10,9 +9,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'login.html'));
-});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
