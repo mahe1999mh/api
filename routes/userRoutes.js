65a73c2fc914e0649151083b1a7cfe2c9df3d066
mahe1999mh <mahe1999mh@gmail.com>
2024-04-14 15:01:20 +0530

    added login api

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,6 +10,6 @@ const router = express.Router();
 
 // router.route("/").get(protect, allUsers);
 router.route("/").post(registerUser);
-// router.post("/login", authUser);
+router.post("/login", authUser);
 
 module.exports = router;
